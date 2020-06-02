02312f0da71f15f9b2a96f9708d14af53b7b9747
Rahul Tarak <rahultarak12345@gmail.com>
2020-06-02 06:14:14 +0530

    Bootstrap Design Update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = document.getElementById('message');
+const message = document.getElementById("message");
 const { createFFmpeg } = FFmpeg;
 const ffmpeg = createFFmpeg({
   log: true,
@@ -7,19 +7,25 @@ const ffmpeg = createFFmpeg({
   },
 });
 
-const transcode = async ({ target: { files }  }) => {
+const transcode = async ({ target: { files } }) => {
   const { name } = files[0];
   await console.log(name);
-  message.innerHTML = 'Loading ffmpeg-core.js';
+  message.innerHTML = "Loading ffmpeg-core.js";
   await ffmpeg.load();
-  message.innerHTML = 'Start transcoding';
-  await console.log("it works")
+  message.innerHTML = "Start transcoding";
+  await console.log("it works");
   await ffmpeg.write(name, files[0]);
-  await ffmpeg.transcode(name,  'output.mp4', '-threads 6');
-  message.innerHTML = 'Complete transcoding';
-  const data = ffmpeg.read('output.mp4');
- 
-  const video = document.getElementById('output-video');
-  video.src = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
-}
-document.getElementById('uploader').addEventListener('change', transcode);
+  await ffmpeg.transcode(name, "output.mp4", "-threads 6");
+  message.innerHTML = "Complete transcoding";
+  const data = ffmpeg.read("output.mp4");
+
+  const video = document.getElementById("output-video");
+  // const videoSrc = document.getElementById("video-src");
+  const download = document.getElementById("download");
+  download.style.display = "block";
+  video.style.display = "block";
+  blobUrl = URL.createObjectURL(new Blob([data.buffer], { type: "video/mp4" }));
+  video.src = blobUrl;
+  download.href = blobUrl;
+};
+document.getElementById("uploader").addEventListener("change", transcode);
