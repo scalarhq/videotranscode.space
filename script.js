d8ab8c7b3a9e396e2a2e865c78d7bd4c244df231
Rahul Tarak <rahultarak12345@gmail.com>
2020-06-02 07:12:09 +0530

    Dark Theme

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
 const message = document.getElementById("message");
 var t1 = new Terminal();
 t1.setWidth("1130px");
-t1.setBackgroundColor("black");
+t1.setBackgroundColor("#263238");
+t1.setTextColor("#76ff03");
 t1.blinkingCursor(true);
 let terminalDiv = document.getElementById("terminal");
 terminalDiv.appendChild(t1.html);
-t1.print("Hello, I am an FFmpeg Wasm Video Editor");
+t1.print("Hello, I am an FFmpeg Wasm Video Transcoder");
 
 const { createFFmpeg } = FFmpeg;
 const ffmpeg = createFFmpeg({
   log: true,
   progress: ({ ratio }) => {
-    t1.print(`Complete: ${(ratio * 100.0).toFixed(2)}%`);
+    let value = (ratio * 100.0).toFixed(2);
+    if (value > 0) {
+      t1.print(`Complete: ${value}%`);
+    }
   },
 });
 
 const transcode = async ({ target: { files } }) => {
   const { name } = files[0];
   await console.log(name);
-  message.innerHTML = "Loading ffmpeg-core.js";
+  t1.print("Loading ffmpeg-core.js");
   await ffmpeg.load();
-  message.innerHTML = "Start transcoding";
+  t1.print("Start processing");
   await console.log("it works");
   await ffmpeg.write(name, files[0]);
-  await ffmpeg.run(`-i ${name} -vf hue=s=0 output.mp4 -threads 6`);
-  message.innerHTML = "Complete transcoding";
+  let threads = window.navigator.hardwareConcurrency;
+  threads = threads < 8 ? threads : 8;
+  await ffmpeg.run(`-i ${name} -vf hue=s=0 output.mp4 -threads ${threads}`);
+  t1.print("Complete processing");
   const data = ffmpeg.read("output.mp4");
 
   const video = document.getElementById("output-video");
@@ -35,5 +41,7 @@ const transcode = async ({ target: { files } }) => {
   blobUrl = URL.createObjectURL(new Blob([data.buffer], { type: "video/mp4" }));
   video.src = blobUrl;
   download.href = blobUrl;
+  t1.clear();
+  t1.print("The processing is complete! Enjoy your video");
 };
 document.getElementById("uploader").addEventListener("change", transcode);
