7edd76a8cee28e8bbd140326efc9e39ebc4966eb
Rahul Tarak <rahultarak12345@gmail.com>
2020-06-07 05:36:44 +0530

    Added Wasm Logo and Subtitle

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -7,8 +7,16 @@
     font-weight: 400;
     font-size: 46px;
   }
+  h4 {
+    color: white;
+    font-weight: 400;
+  }
 </style>
 
 <div class="header">
-  <h1 class="title">WASM Video Transcoder</h1>
+  <h1 class="title">Browser Based Video Transcoder</h1>
+  <h4>
+    Your files will not be uploaded anywhere, all processing will be done on
+    your browser.
+  </h4>
 </div>
