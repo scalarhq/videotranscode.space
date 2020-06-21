573239b4e442fc3548debdad053946efe7c74aa3
Rahul Tarak <rahultarak12345@gmail.com>
2020-06-21 20:58:50 +0530

    Reverted FFmpeg.js to dev version

--- src/js/ffmpeg.js
+++ src/js/ffmpeg.js
@@ -51,6 +51,7 @@ config.subscribe((value) => {
   if (compressionValue && sliderValue) {
     /** Updates the slider value to the range of the current codec in case it is switched */
     compressionValue = min + ((max - 1) * sliderValue) / 100;
+    console.info(compressionValue);
   }
 
   console.log(
@@ -63,6 +64,7 @@ sliderStore.subscribe((value) => {
   sliderValue = value;
   /** Changes a 0-100 % range to a range between the minimum and maximum values for that codec */
   compressionValue = min + ((max - 1) * sliderValue) / 100;
+  console.info(compressionValue);
 });
 
 /** Triggers when a file is uploaded */
@@ -105,125 +107,22 @@ const ffmpeg = createFFmpeg({
   loadedStore.update((existing) => true);
 })();
 
-/** Function that performs the FFmpeg transcode command on the video */
-let transcode = async ({ target: { files } }) => {
+/** Function that performs FFmpeg operations on the video */
+let operation = async (file) => {
   const start = new Date().getTime();
-
-  let settings = await chooseSettings(files);
-  let name = settings[0];
-  let threads = settings[1];
-  let format = settings[2];
-  let codec = settings[3];
-  let { extension, type, display, defaultCodec } = format;
-
-  let outputCodec = await setCodec(codec, defaultCodec);
-  console.info(outputCodec);
-
-  videoDisplay.update((existing) => display);
-  const output = `${files[0].name}-output${extension}`;
-
-  await ffmpeg.run(
-    `-i ${name} -threads ${threads} ${outputCodec} -strict -2 ${output}`
-  );
-
-  terminalText.update((existing) => "Complete processing");
-  const data = ffmpeg.read(`${output}`);
-
-  createVideo(data, type);
-  /** Gets the time taken to process */
-  const end = new Date().getTime();
-  encodeTime = (end - start) / 1000;
-
-  updateData(encodeTime);
-
-  console.log(
-    `The processing is complete! Enjoy your video. It took ${encodeTime} seconds`
-  );
-};
-
-/** Function that performs the FFmpeg compress command on the video */
-let compression = async ({ target: { files } }) => {
-  const start = new Date().getTime();
-
-  let settings = await chooseSettings(files);
-  let name = settings[0];
-  let threads = settings[1];
-  let format = settings[2];
-
-  let { extension, type, display, defaultCodec } = format;
-  let outputCodec = "";
-
-  /** Compression */
-  let compress = "";
-
-  if (sliderValue > 0) {
-    compress = "-crf " + compressionValue;
-  }
-
-  videoDisplay.update((existing) => display);
-  const output = `${files[0].name}-output${extension}`;
-
-  await ffmpeg.run(
-    `-i ${name} -threads ${threads} ${outputCodec} -strict -2 ${output} ${compress}`
-  );
-
-  terminalText.update((existing) => "Complete processing");
-  const data = ffmpeg.read(`${output}`);
-
-  createVideo(data, type);
-  /** Gets the time taken to process */
-  const end = new Date().getTime();
-  encodeTime = (end - start) / 1000;
-
-  updateData(encodeTime);
-
-  console.log(
-    `The processing is complete! Enjoy your video. It took ${encodeTime} seconds`
-  );
-};
-
-let setCodec = async (codec, defaultCodec) => {
-  let outputCodec = "";
-  console.log(codec);
-  console.log(defaultCodec);
-  if (codec) {
-    console.info("Selected Codec", codec);
-    let ffmpegLib = codec.ffmpegLib;
-    console.info(ffmpegLib);
-    outputCodec = `-c:v ${ffmpegLib}`;
-    console.info(outputCodec);
-  } else if (defaultCodec) {
-    console.info("Default Codec", defaultCodec);
-    let ffmpegLibs = defaultCodec.ffmpegLibs;
-    outputCodec = `-c:v ${ffmpegLibs}`;
-  }
-  return outputCodec;
-};
-
-/** The function that allows users to select formats and codecs */
-const chooseSettings = async (files) => {
-  let settings = [];
-
-  const { name } = files[0];
-  /** Index 0 is the name */
-  settings.push(name);
-
+  const { name } = file;
   await console.info(name);
+
   terminalText.update((existing) => "Start processing");
-  await ffmpeg.write(name, files[0]);
+  await ffmpeg.write(name, file);
   /** Get the number of threads being used */
   let threads = getThreads();
 
-  /** Index 1 is the number of threads */
+  let grayscale = false;
   threads = threads < 8 ? threads : 8;
-  settings.push(threads);
 
   console.info("Configuration", configuration);
   let { format, codec } = configuration;
-  /** Index 2 is the format */
-  settings.push(format);
-  /** Index 3 is the chosen codec */
-  settings.push(codec);
 
   console.log(
     `The final settings are fileType ${format.name} with ${codec.name} and a compression level of ${sliderValue}%`
@@ -231,16 +130,50 @@ const chooseSettings = async (files) => {
   finalSettings.format = format.name;
   finalSettings.codec = codec.name;
 
-  return settings;
-};
+  let { extension, type, display, defaultCodec } = format;
 
-/** Creates the video object */
-const createVideo = (data, type) => {
+  let outputCodec = "";
+
+  let params = "";
+
+  params += grayscale ? `-vf hue=s=0` : "";
+
+  /** Compression */
+  let compress;
+
+  if (sliderValue > 0) {
+    /** TODO: Replace with getFFmpegFlags command from configuration.js once it's finished */
+    compress = "-crf " + compressionValue;
+  }
+
+  videoDisplay.update((existing) => display);
+
+  const output = `${name}-output${extension}`;
+
+  await ffmpeg.run(
+    `-i '${name}'  ${params} -threads ${threads} ${outputCodec} -strict -2 ${output} ${
+      compress ? compress : ""
+    }`
+  );
+
+  terminalText.update((existing) => "Complete processing");
+  const data = ffmpeg.read(`${output}`);
+  /** Creates the video object */
+
   let blobUrl = URL.createObjectURL(new Blob([data.buffer], { type: type }));
   console.info(blobUrl);
   transcoded.update((existing) => blobUrl);
   clearTerminal.update((existing) => true);
   processed.update((existing) => true);
+  /** Gets the time taken to process */
+  const end = new Date().getTime();
+  encodeTime = (end - start) / 1000;
+
+  updateData(encodeTime);
+
+  console.log(
+    `The processing is complete! Enjoy your video. It took ${encodeTime} seconds`
+  );
 };
 
 const updateData = (encodeTime) => {
@@ -269,10 +202,6 @@ const updateData = (encodeTime) => {
 /** Triggers once the submit button is pressed */
 submit.subscribe((value) => {
   if (value) {
-    /**if (compressionValue > 0) {
-      compress(fileInput);
-    }**/
-
-    transcode(fileInput);
+    operation(fileInput);
   }
 });
