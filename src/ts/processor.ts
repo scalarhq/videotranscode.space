5a4fa088a538e807c2231c2cdcadfe20be5bcc7b
Rahul Tarak <rahultarak12345@gmail.com>
2020-06-27 20:20:44 +0530

    Configuration Abstraction

--- src/ts/processor.ts
+++ src/ts/processor.ts
@@ -3,9 +3,7 @@ import {
   terminalText,
   clearTerminal,
   processed,
-  config,
   submit,
-  sliderStore,
 } from "../store/stores";
 import "../ts/form";
 
@@ -15,37 +13,9 @@ import { handleNewTranscode } from "./transcode";
 import { handleNewCompression } from "./compression";
 import { updateData, getThreads } from "./hardware";
 import { fileInput, fileData } from "./file";
+import { finalConfiguration } from "./configuration";
 
-let configuration: ConfigurationType;
-let min = 0;
-let max = 0;
-let sliderValue = 0;
-let compressionValue = 0;
-
-/** Triggers whenever the codec or format is changed */
-config.subscribe((value: any) => {
-  configuration = value;
-  let currentCodec = configuration.codec;
-  min = currentCodec.compressionRange.min;
-  max = currentCodec.compressionRange.max;
-
-  if (sliderValue > 0 && compressionValue > 0) {
-    /** Updates the slider value to the range of the new codec in case the codec is switched */
-    compressionValue = min + ((max - 1) * sliderValue) / 100;
-  }
-
-  console.log(
-    `The new file format is ${configuration.format.name} and the chosen codec is ${configuration.codec.name}`
-  );
-});
-
-/** Triggers whenever the slider is moved */
-sliderStore.subscribe((value) => {
-  sliderValue = value;
-  console.log(`The new compression level is ${sliderValue}%`);
-  /** Changes a 0-100 % range to a range between the minimum and maximum values for the chosen codec */
-  compressionValue = min + ((max - 1) * sliderValue) / 100;
-});
+const { configuration, sliderValue, compressionValue } = finalConfiguration;
 
 const handleSubmit = async () => {
   const start = new Date().getTime();
@@ -112,6 +82,9 @@ const handleSubmit = async () => {
   const end = new Date().getTime();
   const encodeTime = (end - start) / 1000;
   updateData(encodeTime, fileData, finalSettings);
+  console.log(
+    `The processing is complete! Enjoy your video. It took ${encodeTime} seconds`
+  );
 };
 
 const createVideoObject = (processedFile: any, videoType: string) => {
