12a86d63d003fd981778eebbbaac1c4df1334081
Rahul Tarak <rahultarak12345@gmail.com>
2020-07-12 17:01:57 +0530

    :video_camera:  Moving Codecs and Formats

---
+++ codecs/mpeg4.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "MPEG-4",
+  compressionRange: {
+    min: 1,
+    max: 31,
+  },
+  ffmpegLib: "libxvid",
+  notSupported: true,
+};
---
+++ codecs/windows.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "Windows Media Video",
+    compressionRange: {
+      min: 1,
+      max: 51,
+    },
+    ffmpegLib: "wmv2"
+}
