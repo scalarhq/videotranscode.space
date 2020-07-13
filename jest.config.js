59f4b23f183df915c29f40051c46d47508a190ab
Rahul Tarak <rahultarak12345@gmail.com>
2020-07-13 09:22:44 +0530

    ⚙ Svelte Testing Configuration

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
+  transform: {
+    "^.+\\.svelte$": "svelte-jester",
+    "^.+\\.ts$": "ts-jest",
+    "^.+\\.js$": "babel-jest",
+  },
+  moduleFileExtensions: ["js", "svelte", "ts"],
   setupFilesAfterEnv: ["./jest.setup.js"],
 };
