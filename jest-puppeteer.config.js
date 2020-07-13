3602c8f0ff3db07c708322b30e05071173293808
Cryogenic <rahultarak12345@gmail.com>
2020-07-13 20:33:54 +0530

    ⚙️ Mac Puppeteer Configg

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  launch: {
+    // dumpio: true,
+    headless: false,
+  },
+  //browser: 'chromium',
+  //browserContext: 'default',
+  executablePath:
+    "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+};
