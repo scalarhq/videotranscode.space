6af4b63410a3689d789a77b18f0491aad9c6f56e
repl.it user <replituser@example.com>
2020-07-14 19:16:00 +0000

    Setup Loader in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Loader from "./components/loader"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [loaded, setLoaded] = useState(false)
+  if (loaded) {
+		return <Loader/>
+	} else {
+		return (<main> </main>)
+	}
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import store from './store/store'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+	 <Provider store={store}>
     <App />
+		</Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
