9a854f4a1d968179b363a0414e4bfb0f6613b02f
Rahul Tarak <rahultarak12345@gmail.com>
2020-07-31 05:16:35 +0530

    Commented out JSDOM Mocking

--- src/ts/ffmpeg.ts
+++ src/ts/ffmpeg.ts
@@ -1,6 +1,9 @@
 // @ts-ignore
-// import { createFFmpeg } from '@ffmpeg/ffmpeg';
-// import { loadedStore, progressStore } from '../store/stores';
+import { createFFmpeg } from '@ffmpeg/ffmpeg';
+import ComponentStore from '../store/componentStore';
+
+const { ProgressStore } = ComponentStore;
+const { updateProgress } = ProgressStore;
 
 export type FFmpegDataType = {
   outputFileName: string;
@@ -18,94 +21,92 @@ const exportedElements: {
  * Mocking Behavior for JS DOM for Testing Purposes
  */
 
-if (window.navigator.userAgent.includes('jsdom')) {
-  // This is a testing environment and not a production environment.
+// if (window.navigator.userAgent.includes('jsdom')) {
+//   // This is a testing environment and not a production environment.
 
-  // Mock Functions
+//   // Mock Functions
 
-  const mockWriter = async (file: File) => 'mock-name';
-  const mockReader = async (filename: string) => {
-    const newMockArray = new Uint8Array();
-    return newMockArray;
-  };
-  const mockRunner = async (fileName: string, ffmpegData: FFmpegDataType) => 'processed-mock-name';
-  exportedElements.ffmpegRunner = mockRunner;
-  exportedElements.ffmpegReader = mockReader;
-  exportedElements.ffmpegWriter = mockWriter;
-} else {
-  /** *
-   * Creates an FFmpeg Instance and Updates the progress bar with progress value
-   */
-  // const ffmpeg = createFFmpeg({
-  //   log: true,
-  //   progress: (input: any) => {
-  //     const value: number = input.ratio * 100.0;
-  //     if (value > 0) {
-  //       console.info(`Completed ${value.toFixed(2)}%`);
-  //       // progressStore.update(() => value);
-  //     }
-  //   },
-  // });
-  // @ts-ignore
-  const ffmpeg: any;
-
-  /** Checks if FFmpeg is supported on that browser */
-  (async () => {
-    try {
-      await ffmpeg.load();
-    } catch (err) {
-      console.error(window.navigator.userAgent, err.message);
-      // eslint-disable-next-line no-alert
-      alert(`Your Browser is not supported ${err.message}`);
+//   const mockWriter = async (file: File) => 'mock-name';
+//   const mockReader = async (filename: string) => {
+//     const newMockArray = new Uint8Array();
+//     return newMockArray;
+//   };
+//   const mockRunner = async (fileName: string, ffmpegData: FFmpegDataType) => 'processed-mock-name';
+//   exportedElements.ffmpegRunner = mockRunner;
+//   exportedElements.ffmpegReader = mockReader;
+//   exportedElements.ffmpegWriter = mockWriter;
+// } else {
+/** *
+ * Creates an FFmpeg Instance and Updates the progress bar with progress value
+ */
+const ffmpeg = createFFmpeg({
+  log: true,
+  progress: (input: any) => {
+    const value: number = input.ratio * 100.0;
+    if (value > 0) {
+      console.info(`Completed ${value.toFixed(2)}%`);
+      updateProgress(value);
     }
-    // loadedStore.update(() => true);
-  })();
+  },
+});
 
-  /** *
-   * FFmpeg Writer Loads a Video in WASM Memory for FFmpeg to use later
-   * @param file is the actual inputted file from the user
-   * @retuns The name of the file the user added
-   */
+/** Checks if FFmpeg is supported on that browser */
+(async () => {
+  try {
+    await ffmpeg.load();
+  } catch (err) {
+    console.error(window.navigator.userAgent, err.message);
+    // eslint-disable-next-line no-alert
+    alert(`Your Browser is not supported ${err.message}`);
+  }
+  // loadedStore.update(() => true);
+})();
 
-  const ffmpegWriter = async (file: File) => {
-    const { name } = file;
-    await ffmpeg.write(name, file);
-    return name;
-  };
+/** *
+ * FFmpeg Writer Loads a Video in WASM Memory for FFmpeg to use later
+ * @param file is the actual inputted file from the user
+ * @retuns The name of the file the user added
+ */
 
-  /** *
-   * FFmpeg Reader Reads a Video from WASM Memory into JavaScript
-   * @param fileName is the file name of the video which would like to be read
-   * @return Uint8Array of data containing the file
-   */
+const ffmpegWriter = async (file: File) => {
+  const { name } = file;
+  await ffmpeg.write(name, file);
+  return name;
+};
 
-  const ffmpegReader = async (fileName: string) => {
-    const processedFile: Uint8Array = ffmpeg.read(`${fileName}`);
-    return processedFile;
-  };
+/** *
+ * FFmpeg Reader Reads a Video from WASM Memory into JavaScript
+ * @param fileName is the file name of the video which would like to be read
+ * @return Uint8Array of data containing the file
+ */
 
-  /** *
-   * FFmpeg Runner Executes an FFmpeg Command in WASM
-   * @param fileName is the name of the file on which the command is executed.
-   * @param ffmpegData is the parameters of the executed and are of type {@link FFmpegDataType}
-   * @return a file name as string of the processed file.
-   */
+const ffmpegReader = async (fileName: string) => {
+  const processedFile: Uint8Array = ffmpeg.read(`${fileName}`);
+  return processedFile;
+};
 
-  const ffmpegRunner = async (fileName: string, ffmpegData: FFmpegDataType) => {
-    const { outputFileName, threads, ffmpegCommands } = ffmpegData;
-    await ffmpeg.run(
-      `-i '${fileName}' -threads ${threads} ${ffmpegCommands} -strict -2 ${outputFileName} 
+/** *
+ * FFmpeg Runner Executes an FFmpeg Command in WASM
+ * @param fileName is the name of the file on which the command is executed.
+ * @param ffmpegData is the parameters of the executed and are of type {@link FFmpegDataType}
+ * @return a file name as string of the processed file.
+ */
+
+const ffmpegRunner = async (fileName: string, ffmpegData: FFmpegDataType) => {
+  const { outputFileName, threads, ffmpegCommands } = ffmpegData;
+  await ffmpeg.run(
+    `-i '${fileName}' -threads ${threads} ${ffmpegCommands} -strict -2 ${outputFileName} 
       }`
-    );
-    return outputFileName;
-  };
+  );
+  return outputFileName;
+};
 
-  // Exporting the Above Functions
-  exportedElements.ffmpegRunner = ffmpegRunner;
-  exportedElements.ffmpegReader = ffmpegReader;
-  exportedElements.ffmpegWriter = ffmpegWriter;
-}
+// Exporting the Above Functions
+// exportedElements.ffmpegRunner = ffmpegRunner;
+// exportedElements.ffmpegReader = ffmpegReader;
+// exportedElements.ffmpegWriter = ffmpegWriter;
+// }
 
-const { ffmpegRunner, ffmpegReader, ffmpegWriter } = exportedElements;
+// const { ffmpegRunner, ffmpegReader, ffmpegWriter } = exportedElements;
 
 export { ffmpegRunner, ffmpegReader, ffmpegWriter };
