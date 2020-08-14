1a4ad31c30d17fa0427f3cd66e9f600f982693cd
Rahul Tarak <rahultarak12345@gmail.com>
2020-08-14 22:13:19 +0530

    🔥 Basic Feature Tool

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-nested-ternary */
 
 // Modules
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { observer } from 'mobx-react';
 // @ts-ignore
 import Fade from 'react-reveal/Fade';
@@ -21,6 +21,7 @@ import ProgressBar from './components/progress/progress';
 import VideoPlayer from './components/video/video';
 import ErrorScreen from './components/error/Error';
 import Clui from './clui/clui';
+import BasicFeatures from './components/basic-features/basicFeature';
 
 // Types
 import { ComponentStoreType } from './types/store';
@@ -54,7 +55,7 @@ const App: React.FC<AppProps> = ({ componentStore }: AppProps) => {
     loadFFmpeg();
   }, []);
 
-  // const [processing, updateProcessing] = useState(false);
+  const [cluiToggle, setCluiToggle] = useState(true);
 
   useEffect(() => {
     if (isSubmitted) {
@@ -119,9 +120,108 @@ const App: React.FC<AppProps> = ({ componentStore }: AppProps) => {
               )
               : !isSubmitted ? (
 
-                <Fade bottom>
-                  <Clui />
-                </Fade>
+                <div className="configuration-wrapper">
+                  <Fade bottom>
+                    {cluiToggle ? <Clui /> : <BasicFeatures />}
+                  </Fade>
+                  <div className="toggle">
+                    <div className="toggle-label">
+                      <p>Basic Features</p>
+                    </div>
+                    <label className="switch">
+                      <input
+                        type="checkbox"
+                        checked={cluiToggle}
+                        onChange={(e) => {
+                          setCluiToggle(!cluiToggle);
+                        }}
+                      />
+                      <span className="toggle-slider round" />
+                    </label>
+                    <div className="toggle-label">
+                      <p>All Features (CLUI)</p>
+                    </div>
+                  </div>
+                  {/* @ts-ignore Styled JSX */}
+                  <style jsx>
+                    {`
+                    .configuration-wrapper {
+                      display : flex;
+                      flex-direction : column;
+                    }
+                    .toggle {
+                      display: flex;
+                      flex-direction: row;
+                      justify-content: center;
+                      align-items: center;
+                    }
+                    .toggle-label {
+                      padding: 0 2%;
+                      font-weight: 400;
+                    }
+
+                    .switch {
+                      position: relative;
+                      display: inline-block;
+                      width: 60px;
+                      height: 34px;
+                    }
+
+                    .switch input {
+                      opacity: 0;
+                      width: 0;
+                      height: 0;
+                    }
+
+                    .toggle-slider {
+                      position: absolute;
+                      cursor: pointer;
+                      top: 0;
+                      left: 0;
+                      right: 0;
+                      bottom: 0;
+                      background-color: #ccc;
+                      -webkit-transition: 0.4s;
+                      transition: 0.4s;
+                    }
+
+                    .toggle-slider:before {
+                      position: absolute;
+                      content: '';
+                      height: 26px;
+                      width: 26px;
+                      left: 4px;
+                      bottom: 4px;
+                      background-color: white;
+                      -webkit-transition: 0.4s;
+                      transition: 0.4s;
+                    }
+
+                    input:checked + .toggle-slider {
+                      background-color: #2196f3;
+                    }
+
+                    input:focus + .toggle-slider {
+                      box-shadow: 0 0 1px #2196f3;
+                    }
+
+                    input:checked + .toggle-slider:before {
+                      -webkit-transform: translateX(26px);
+                      -ms-transform: translateX(26px);
+                      transform: translateX(26px);
+                    }
+
+                    /* Rounded sliders */
+                    .toggle-slider.round {
+                      border-radius: 34px;
+                    }
+
+                    .toggle-slider.round:before {
+                      border-radius: 50%;
+                    }
+                    `}
+                  </style>
+                </div>
 
               ) : !processed ? (
                 <Fade bottom>
