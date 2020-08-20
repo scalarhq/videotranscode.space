227232c3358342c1404d5939f1b609e9a552c2af
Rahul Tarak <rahultarak12345@gmail.com>
2020-08-21 04:28:23 +0530

    ⚠ Less threatening error page - Automatic Issue Link - Added more data and template to this issue

--- src/components/error/Error.tsx
+++ src/components/error/Error.tsx
@@ -1,22 +1,42 @@
 import React from 'react';
+import newGithubIssueUrl from 'new-github-issue-url';
 
 const ErrorScreen = ({ loadingErrorObj }: { loadingErrorObj: Error }) => (
   <div className="error-wrapper">
+    <h1 style={{ color: 'white' }}>Oops! Something went wrong</h1>
     <div className="error-icon">
-      <svg viewBox="0 0 32.00199890136719 32" width="24vh" height="24vh"><path xmlns="http://www.w3.org/2000/svg" d="M2.062 32h27.812a2 2 0 0 0 1.766-2.942l-13.876-26A1.997 1.997 0 0 0 16.002 2H16c-.738 0-1.414.406-1.762 1.056L.3 29.056a2.004 2.004 0 0 0 .046 1.972A2.005 2.005 0 0 0 2.062 32zM16 24a2 2 0 1 1-.001 4.001A2 2 0 0 1 16 24zm-2-3.968v-8a2 2 0 0 1 4 0v8a2 2 0 0 1-4 0z" /></svg>
+      {/* <svg viewBox="0 0 32.00199890136719 32" width="24vh" height="24vh"><path xmlns="http://www.w3.org/2000/svg" d="M2.062 32h27.812a2 2 0 0 0 1.766-2.942l-13.876-26A1.997 1.997 0 0 0 16.002 2H16c-.738 0-1.414.406-1.762 1.056L.3 29.056a2.004 2.004 0 0 0 .046 1.972A2.005 2.005 0 0 0 2.062 32zM16 24a2 2 0 1 1-.001 4.001A2 2 0 0 1 16 24zm-2-3.968v-8a2 2 0 0 1 4 0v8a2 2 0 0 1-4 0z" /></svg> */}
+      <svg xmlns="http://www.w3.org/2000/svg" width="24vh" height="24vh" viewBox="0 0 24 24">
+        <g data-name="Layer 2">
+          <g data-name="alert-circle">
+            <rect width="24" height="24" opacity="0" />
+            <path d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm0 18a8 8 0 1 1 8-8 8 8 0 0 1-8 8z" />
+            <circle cx="12" cy="16" r="1" />
+            <path d="M12 7a1 1 0 0 0-1 1v5a1 1 0 0 0 2 0V8a1 1 0 0 0-1-1z" />
+          </g>
+        </g>
+      </svg>
     </div>
     <div className="error-message">
-      <h3 style={{ color: '#ff3e00' }}>Oops! Looks like there has been an error.</h3>
-      <p>
-        Check the error message below and if you think something is wrong, please create an issue
-        {' '}
-        <a href="https://github.com/Mozilla-Open-Lab-Etwas/Video-Transcoder/issues/new">here</a>
-      </p>
-      <p style={{ color: 'white' }}>{loadingErrorObj.message}</p>
-      <p style={{ color: 'white' }}>
+
+      <p style={{ color: 'white' }}>{loadingErrorObj?.message ? loadingErrorObj.message : 'Unknown Error'}</p>
+      <p style={{ color: 'white', fontSize: '14px' }}>
         Your current browser version is
         {' '}
         {window.navigator.userAgent}
+      </p>
+      <p>
+        Check the error message above and if you think something is wrong, please create an issue
+        {' '}
+        <a
+          href={newGithubIssueUrl({
+            user: 'Etwas-Builders', repo: 'Video-Transcoder', body: `\n\n\n-----\n Browser Version: ${window.navigator.userAgent} \n\n-----\n **Error Message : ${loadingErrorObj?.message ? loadingErrorObj.message : 'Unknown Error'}**`, title: '[BUG] [CRASH]', labels: ['bug', 'crash-report'],
+          })}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          here
+        </a>
       </p>
     </div>
     {/* @ts-ignore Styled JSX */}
@@ -28,7 +48,7 @@ const ErrorScreen = ({ loadingErrorObj }: { loadingErrorObj: Error }) => (
         align-items: center;
       }
       .error-icon {
-        fill: #ff3e00
+        fill: grey;
       }
       .error-message {
         display: flex;
