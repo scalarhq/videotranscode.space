be118d88d1f16aca828960299a2e1d004a2e428b
Rahul Tarak <rahultarak12345@gmail.com>
2020-08-23 18:05:14 +0530

    🔐 Removed Strict Mode - Dependancy uses old context api and old class components - Might replace or fork and upgrade this module

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <>
     {/* <Provider store={store}> */}
     <App />
     {/* </Provider> */}
-  </React.StrictMode>,
+  </>,
   document.getElementById('root'),
 );
 
