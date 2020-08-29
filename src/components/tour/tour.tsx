3ade6eaec11cd389ebc0eed2a8cb8eca6611f45d
Rahul Tarak <rahultarak12345@gmail.com>
2020-08-29 19:30:04 +0530

    📕 Guided Tour!

---
+++ src/components/tour/tour.tsx
@@ -0,0 +1,45 @@
+import React, { useContext, useEffect } from 'react';
+
+import { ShepherdTour, ShepherdTourContext } from 'react-shepherd';
+
+import steps from './steps';
+
+import 'shepherd.js/dist/css/shepherd.css';
+
+const tourOptions = {
+  defaultStepOptions: {
+    cancelIcon: {
+      enabled: true,
+    },
+  },
+  useModalOverlay: true,
+  scrollTo: { behavior: 'smooth', block: 'center' },
+};
+
+type TourProps = { children: JSX.Element, landing: boolean }
+
+const Tour = ({ children, landing }: TourProps) => {
+  const tour = useContext(ShepherdTourContext);
+
+  useEffect(() => {
+    console.info('Updated landing', landing);
+    if (!landing) {
+      console.info('Starting Tour!');
+      // @ts-ignore
+      tour.start();
+    }
+  }, [landing]);
+
+  return (
+    <div>{children}</div>
+  );
+};
+
+const TourWrapper = ({ children, landing }: TourProps) => (
+  // @ts-ignore
+  <ShepherdTour steps={steps} tourOptions={tourOptions}>
+    <Tour landing={landing}>{children}</Tour>
+  </ShepherdTour>
+);
+
+export default TourWrapper;
