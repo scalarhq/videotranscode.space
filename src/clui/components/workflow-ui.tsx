3be910dd92c0a4b18bdd4790da208cbcfcb6711b
Rahul Tarak <rahultarak12345@gmail.com>
2020-10-27 04:29:04 +0530

    🏗 Display subFeatures recursively

--- src/clui/components/workflow-ui.tsx
+++ src/clui/components/workflow-ui.tsx
@@ -5,17 +5,33 @@ import Submit from '../../clui-ui-components/Submit';
 import './clui-ui.css';
 
 import ComponentStore from '../../store/componentStore';
+import { WorkflowStep } from '../../dist/workflow';
 
 const { CluiStore } = ComponentStore;
 
 const { updateChosenFeatures } = CluiStore;
+
+
+const displaySubFeatures = (featureKey: WorkflowStep) => {
+  const featuresInUse: Array<FeatureElement> = [];
+  if (featureKey.name === "COMBINED_EXEC_FEATURE") {
+    const { configuration } = featureKey
+
+    for (const subFeature of configuration?.steps || []) {
+      featuresInUse.push(features[subFeature.name]);
+      featuresInUse.push(...displaySubFeatures(subFeature))
+    }
+  }
+  console.info('Returning display sub features', featureKey.name, featuresInUse)
+  return featuresInUse;
+}
 
 /**
  * Creates a UI of Features for each workflow, with the proper wrappers and a Submit button
  *  Maps every step in a workflow to a feature then to a UI Element and displays that
  * @param param0 Expects steps of the workflow as keys of features
  */
-const WorkflowUi = ({ steps }: { steps: Array<string> }) => {
+const WorkflowUi = ({ steps }: { steps: Array<WorkflowStep> }) => {
   useEffect(() => {
     updateChosenFeatures(steps);
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -23,7 +39,8 @@ const WorkflowUi = ({ steps }: { steps: Array<string> }) => {
 
   const featuresInUse: Array<FeatureElement> = [];
   for (const featureKey of steps) {
-    featuresInUse.push(features[featureKey as keyof typeof features]);
+    featuresInUse.push(features[featureKey.name]);
+    featuresInUse.push(...displaySubFeatures(featureKey))
   }
   const UiElements = featuresInUse.map((feature) => feature.ui);
   return (
