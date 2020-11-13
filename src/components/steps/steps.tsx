1d764d27509b977fd67cea321830efd484672d33
Rahul Tarak <cryogenicplanet@gmail.com>
2020-11-13 19:04:19 +0530

    🔀 Intergrate new banner

--- src/components/steps/steps.tsx
+++ src/components/steps/steps.tsx
@@ -38,11 +38,27 @@ const StepComponent = () => {
 
   const [statues, setStatues] = useState<Statuses>(defaultStatus)
 
-  const { processed, globalReset, CluiStore, FileStore } = ComponentStore
+  const {
+    processed,
+    globalReset,
+    CluiStore,
+    FileStore,
+    UserStore
+  } = ComponentStore
 
   const { allFiles } = FileStore
 
   const { isSubmitted } = CluiStore
+
+  const { isActiveUser, localUsageCounter } = UserStore
+
+  const [displayable, setDisplay] = useState(isActiveUser)
+
+  useEffect(() => {
+    if (localUsageCounter && localUsageCounter >= 3) {
+      setDisplay(isActiveUser)
+    }
+  }, [localUsageCounter, isActiveUser])
 
   useEffect(() => {
     if (allFiles.length > 0) {
@@ -71,53 +87,56 @@ const StepComponent = () => {
       setCurrent(3)
     }
   }, [processed])
-
-  return (
-    <div className="step-wrapper">
-      <Steps current={current}>
-        <Step
-          title="Add File"
-          icon={<FontAwesomeIcon icon={faFile} />}
-          status={statues.file}
-        />
-        <Step
-          title="Choose Settings"
-          icon={<FontAwesomeIcon icon={faCogs} />}
-          status={statues.settings}
-        />
-        <Step
-          title="Processing"
-          icon={<FontAwesomeIcon icon={faClock} />}
-          status={statues.processing}
-        />
-        <Step
-          title="Download"
-          icon={<FontAwesomeIcon icon={faDownload} />}
-          status={statues.download}
-        />
-      </Steps>
-      {/* @ts-ignore Styled JSX */}
-      <style jsx>
-        {`
-          .step-wrapper {
-            color: inherit !important;
-            padding-top: 2vh;
-          }
-          .rc-steps-item-title {
-            color: inherit !important;
-          }
-          .rc-steps-item-finish .rc-steps-item-icon > .rc-steps-icon {
-            color: #6c63ff;
-          }
-          .rc-steps-item-finish .rc-steps-item-tail::after {
-            background-color: #6c63ff;
-          }
-          .rc-steps-item-finish .rc-steps-item-title::after {
-            background-color: #6c63ff;
-          }
-        `}
-      </style>
-    </div>
-  )
+  if (!displayable) {
+    return (
+      <div className="step-wrapper">
+        <Steps current={current}>
+          <Step
+            title="Add File"
+            icon={<FontAwesomeIcon icon={faFile} />}
+            status={statues.file}
+          />
+          <Step
+            title="Choose Settings"
+            icon={<FontAwesomeIcon icon={faCogs} />}
+            status={statues.settings}
+          />
+          <Step
+            title="Processing"
+            icon={<FontAwesomeIcon icon={faClock} />}
+            status={statues.processing}
+          />
+          <Step
+            title="Download"
+            icon={<FontAwesomeIcon icon={faDownload} />}
+            status={statues.download}
+          />
+        </Steps>
+        {/* @ts-ignore Styled JSX */}
+        <style jsx>
+          {`
+            .step-wrapper {
+              color: inherit !important;
+              padding-top: 2vh;
+            }
+            .rc-steps-item-title {
+              color: inherit !important;
+            }
+            .rc-steps-item-finish .rc-steps-item-icon > .rc-steps-icon {
+              color: #6c63ff;
+            }
+            .rc-steps-item-finish .rc-steps-item-tail::after {
+              background-color: #6c63ff;
+            }
+            .rc-steps-item-finish .rc-steps-item-title::after {
+              background-color: #6c63ff;
+            }
+          `}
+        </style>
+      </div>
+    )
+  } else {
+    return null
+  }
 }
 export default observer(StepComponent)
