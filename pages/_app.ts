7b3594476355c4fe7e106fca14b5bc0c4b670626
Rahul Tarak <cryogenicplanet@gmail.com>
2020-11-29 17:58:17 +0530

    💄 Global styling - Will change later to css modules

---
+++ pages/_app.ts
@@ -0,0 +1,17 @@
+import '../styles/tailwind.css'
+import '../styles/fonts.css'
+import '../styles/app.css'
+import '../styles/basicFeature.css'
+import '../styles/clui.css'
+import '../styles/configuration.css'
+import '../styles/dropzone.css'
+import '../styles/list.css'
+import '../styles/static.css'
+import '../styles/terminal.css'
+import '../styles/steps.css'
+
+import 'rc-steps/assets/index.css'
+
+import App from 'next/app'
+
+export default App
