793dd8517848b9acef51de9e5b9c5cc8387d0c22
patheticGeek <geekpathetic@gmail.com>
2021-04-24 14:35:40 +0530

    💩 Try local netlify package to install pnpm

---
+++ plugins/pnpm-preinstaller/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  onPreBuild: async ({ utils: { build, run } }) => {
+    try {
+      if (process.env.CI)
+        await run.command(
+          'npx pnpm install -r --store=node_modules/.pnpm-store'
+        )
+      else await run.command('echo skipping pnpm install')
+      await run.command('pnpm run generate')
+    } catch (e) {
+      build.failBuild(e.message)
+    }
+  }
+}
